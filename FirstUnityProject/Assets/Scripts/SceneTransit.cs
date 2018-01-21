3e5b9a89ce2556aa118b7f814ccd3addecbe7392
Samuel <bluecubestuff@gmail.com>
2018-01-21 16:17:10 +0800

    first commit

---
+++ FirstUnityProject/Assets/Scripts/SceneTransit.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransit : MonoBehaviour {
+
+    [SerializeField]
+    Canvas mainMenu;
+
+    [SerializeField]
+     Canvas optionsCanvas;
+
+    Canvas currentCanvas;
+
+
+
+    protected void Start()
+    {
+        optionsCanvas.enabled = false;
+    }
+    public void Game()
+    {
+        SceneManager.LoadScene("InGame"); //load title of scene
+    }
+
+    public void Options()
+    {
+        mainMenu.enabled = false;
+        optionsCanvas.enabled = true;
+
+        currentCanvas = optionsCanvas;
+    }
+
+    public void HighScore()
+    {
+        mainMenu.enabled = false;
+        //highscoreCanvas.enabled = true;
+
+        //currentCanvas = highscoreCanvas;
+    }
+
+    public void Back() // options back btn, highscore back btn
+    {
+        mainMenu.enabled = true;
+
+        //if option, options canvas disable, 
+        //if highscore, highscore canvas disable
+
+        currentCanvas.enabled = false;
+    }
+
+}
